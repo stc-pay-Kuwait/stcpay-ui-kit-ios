befbfd5355838e8f84f8dfec398ba40846d26b42
Waqas Ahmed <waqas.ahmed@appsgenii.co>
2025-04-08 02:01:58 +0500

    add steps cell and apply fixes on font

---
+++ Sources/CustomModifiers/Cells/StepsCell.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CustomModifiers
+//
+//  Created by Waqas Ahmed on 07/04/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
