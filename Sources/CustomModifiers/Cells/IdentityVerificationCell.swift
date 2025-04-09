820e6152c42f892172c33447ceba749c7bfc891d
Waqas Ahmed <waqas.ahmed@appsgenii.co>
2025-04-09 12:40:14 +0500

    add identity verificaiton cell

---
+++ Sources/CustomModifiers/Cells/IdentityVerificationCell.swift
@@ -0,0 +1,85 @@
+//
+//  IdentityVerificationCell.swift
+//  STCPayKuwait
+//
+//  Created by Waqas Ahmed on 09/04/2025.
+//
+
+import SwiftUI
+
+public struct IdentityVerificationCell: View {
+    public var identityVerification: IdentityVerification
+    public init(identityVerification: IdentityVerification) {
+        self.identityVerification = identityVerification
+    }
+    public var body: some View {
+        ZStack(alignment: .topTrailing) {
+            HStack(alignment: .top, spacing: 8) {
+                VStack {
+                    Image(identityVerification.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 64)
+                        .clipped()
+                        .cornerRadius(8)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text(identityVerification.title)
+                        .appFont(.stcForward(.medium, size: 15))
+                        .foregroundColor(Color.darkGrayTitleColor)
+                    Text(identityVerification.subtitle)
+                        .appFont(.stcForward(.regular, size: 15))
+                        .foregroundStyle(Color.textSecondaryGray)
+                        .multilineTextAlignment(.leading)
+                        .lineSpacing(6)
+                        .padding(.top, 2)
+                }
+                
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .frame(width: 24, height: 24)
+                    .foregroundStyle(Color.stPrimaryOsais)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+            .background(.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 16)
+                    .inset(by: 0.5)
+                    .stroke(Color.optionsBorderGray, lineWidth: 1)
+            )
+            
+            if let tagText = identityVerification.tagText, !tagText.isEmpty {
+                Text(tagText)
+                    .appFont(.stcForward(.medium, size: 11))
+                    .foregroundColor(Color.stPrimaryOsais)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color.stPrimaryOsais, lineWidth: 1.5)
+                    }
+                    .background(Color.white)
+                    .offset(x: -24, y: -10)
+            }
+        }
+        .padding()
+    }
+}
+
+//#Preview {
+//    VStack {
+//        IdentityVerificationCell(identityVerification: IdentityVerification(image: "id_img", title: "ID Proof", subtitle: "Capture a picture of your Kuwait Civil ID carte", tagText: "Faster"))
+//        
+//        IdentityVerificationCell(identityVerification: IdentityVerification(image: "id_card", title: "ID Proof", subtitle: "Capture a picture of your Kuwait Civil ID carte", tagText: nil))
+//    }
+//}
+
+public struct IdentityVerification: Identifiable {
+    public let id = UUID()
+    let image: String
+    let title: String
+    let subtitle: String
+    let tagText: String?
+}
