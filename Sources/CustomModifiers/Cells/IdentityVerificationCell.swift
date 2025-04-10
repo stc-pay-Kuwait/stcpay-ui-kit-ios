1a64ff00d15474bb208360aab8f0540a5880cd52
Waqas Ahmed <waqas.ahmed@appsgenii.co>
2025-04-10 14:35:05 +0500

    add onTap method on Identity verification list item

--- Sources/CustomModifiers/Cells/IdentityVerificationCell.swift
+++ Sources/CustomModifiers/Cells/IdentityVerificationCell.swift
@@ -9,72 +9,80 @@ import SwiftUI
 
 public struct IdentityVerificationCell: View {
     public var identityVerification: IdentityVerification
-    public init(identityVerification: IdentityVerification) {
+    public let onTap: () -> Void
+    public init(identityVerification: IdentityVerification, onTap: @escaping () -> Void) {
         self.identityVerification = identityVerification
+        self.onTap = onTap
     }
     public var body: some View {
-        ZStack(alignment: .topTrailing) {
-            HStack(alignment: .top, spacing: 8) {
-                VStack {
-                    Image(identityVerification.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 64)
-                        .clipped()
-                        .cornerRadius(8)
-                }
-                
-                VStack(alignment: .leading) {
-                    Text(identityVerification.title)
-                        .appFont(.stcForward(.medium, size: 15))
-                        .foregroundColor(Color.darkGrayTitleColor)
-                    Text(identityVerification.subtitle)
-                        .appFont(.stcForward(.regular, size: 15))
-                        .foregroundStyle(Color.textSecondaryGray)
-                        .multilineTextAlignment(.leading)
-                        .lineSpacing(6)
-                        .padding(.top, 2)
-                }
-                
-                Spacer()
-                Image(systemName: "chevron.right")
-                    .frame(width: 24, height: 24)
-                    .foregroundStyle(Color.stPrimaryOsais)
-            }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .topLeading)
-            .background(.white)
-            .overlay(
-                RoundedRectangle(cornerRadius: 16)
-                    .inset(by: 0.5)
-                    .stroke(Color.optionsBorderGray, lineWidth: 1)
-            )
-            
-            if let tagText = identityVerification.tagText, !tagText.isEmpty {
-                Text(tagText)
-                    .appFont(.stcForward(.medium, size: 11))
-                    .foregroundColor(Color.white)
-                    .padding(.horizontal, 10)
-                    .padding(.vertical, 5)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color.purpleBorder, lineWidth: 1.5)
+        Button(action: {
+            onTap()
+        }){
+            ZStack(alignment: .topTrailing) {
+                HStack(alignment: .top, spacing: 8) {
+                    VStack {
+                        Image(identityVerification.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 64)
+                            .clipped()
+                            .cornerRadius(8)
                     }
-                    .background(Color.purpleBody)
-                    .cornerRadius(6)
-                    .offset(x: -24, y: -10)
+                    
+                    VStack(alignment: .leading) {
+                        Text(identityVerification.title)
+                            .appFont(.stcForward(.medium, size: 15))
+                            .foregroundColor(Color.darkGrayTitleColor)
+                        Text(identityVerification.subtitle)
+                            .appFont(.stcForward(.regular, size: 15))
+                            .foregroundStyle(Color.textSecondaryGray)
+                            .multilineTextAlignment(.leading)
+                            .lineSpacing(6)
+                            .padding(.top, 2)
+                    }
+                    
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(Color.stPrimaryOsais)
+                }
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                .background(.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .inset(by: 0.5)
+                        .stroke(Color.optionsBorderGray, lineWidth: 1)
+                )
+                
+                if let tagText = identityVerification.tagText, !tagText.isEmpty {
+                    Text(tagText)
+                        .appFont(.stcForward(.medium, size: 11))
+                        .foregroundColor(Color.white)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 5)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.purpleBorder, lineWidth: 1.5)
+                        }
+                        .background(Color.purpleBody)
+                        .cornerRadius(6)
+                        .offset(x: -24, y: -10)
+                }
             }
         }
     }
 }
 
-#Preview {
-    VStack {
-        IdentityVerificationCell(identityVerification: IdentityVerification(image: "id_img", title: "ID Proof", subtitle: "Capture a picture of your Kuwait Civil ID carte", tagText: "Faster"))
-        
-        IdentityVerificationCell(identityVerification: IdentityVerification(image: "id_card", title: "ID Proof", subtitle: "Capture a picture of your Kuwait Civil ID carte", tagText: nil))
-    }
-}
+//#Preview {
+//    VStack {
+//        IdentityVerificationCell(identityVerification: IdentityVerification(image: "id_img", title: "ID Proof", subtitle: "Capture a picture of your Kuwait Civil ID carte", tagText: "Faster")) {
+//            print("Click on list")
+//        }
+//        
+////        IdentityVerificationCell(identityVerification: IdentityVerification(image: "id_card", title: "ID Proof", subtitle: "Capture a picture of your Kuwait Civil ID carte", tagText: nil))
+//    }
+//}
 
 public struct IdentityVerification: Identifiable {
     public let id = UUID()
