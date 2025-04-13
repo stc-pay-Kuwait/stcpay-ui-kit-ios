4523ec54347224595b195a040e103af87ab0d53b
Waqas Ahmed <waqas.ahmed@appsgenii.co>
2025-04-13 17:05:06 +0500

    update radio button functionality to make it radio button on left or right

--- Sources/CustomModifiers/OptionsFields/RadioOptionsView.swift
+++ Sources/CustomModifiers/OptionsFields/RadioOptionsView.swift
@@ -11,45 +11,29 @@ public struct RadioOptionsView: View {
     let title: String
     let id: String
     @Binding var selectedID: String?
+    let radioPosition: RadioPosition
     
     var isSelected: Bool {
         selectedID == id
     }
     
-    public init(title: String, id: String, selectedID: Binding<String?>) {
+    public init(title: String, id: String, selectedID: Binding<String?>, radioPosition: RadioPosition = .left) {
         self.title = title
         self.id = id
         self._selectedID = selectedID
+        self.radioPosition = radioPosition
     }
     
     public var body: some View {
         Button(action: {
             selectedID = id
         }) {
-            HStack(alignment: title.isSingleLine ? .center : .firstTextBaseline) { // Dynamically adjust alignment
-                ZStack {
-                    Button(action: {
-                        selectedID = id
-                    }) {
-                        ZStack {
-                            Circle()
-                                .stroke(Color.borderGrayPrimary, lineWidth: 1)
-                                .frame(width: 20, height: 20)
-                            
-                            if isSelected {
-                                Circle()
-                                    .fill(Color.stPrimaryOsais)
-                                    .frame(width: 20, height: 20)
-                                Circle()
-                                    .fill(Color.white)
-                                    .frame(width: 8, height: 8)
-                            }
-                        }
-                        .frame(width: 20, height: 20)
-                        .offset(y: title.isSingleLine ? 0 : 5)
-                    }
-                }
+            HStack(alignment: title.isSingleLine ? .center : .firstTextBaseline) {
                 
+                if radioPosition == .left {
+                    radioCircle
+                }
+
                 VStack(alignment: .leading, spacing: 4) { // Text container
                     Text(title)
                         .foregroundColor(.textSecondaryGray)
@@ -60,12 +44,45 @@ public struct RadioOptionsView: View {
                 }
                 
                 Spacer()
+                
+                if radioPosition == .right {
+                    radioCircle
+                }
             }
         }
     }
+    
+    @ViewBuilder
+    private var radioCircle: some  View {
+        ZStack {
+            Circle()
+                .stroke(Color.borderGrayPrimary, lineWidth: 1)
+                .frame(width: 20, height: 20)
+            
+            if isSelected {
+                Circle()
+                    .fill(Color.stPrimaryOsais)
+                    .frame(width: 20, height: 20)
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 8, height: 8)
+            }
+        }
+        .frame(width: 20, height: 20)
+        .offset(y: title.isSingleLine ? 0 : 5)
+    }
+
 }
 
 //#Preview {
 //    @State var selectedRadioID: String?
 //    RadioOptionsView(title: "Enable Notifications Enable Notifications Enable Notifications Enable Notifications Enable Notifications Enable Notifications",  id: "option3", selectedID: $selectedRadioID)
+//    
+//    RadioOptionsView(title: "Enable Notifications",  id: "option3", selectedID: $selectedRadioID, radioPosition: .right)
+//    
 //}
+
+public enum RadioPosition {
+    case left
+    case right
+}
